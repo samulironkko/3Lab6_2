a61a024b9ad3d9bdfd63980d75a61f63ee7decab
Samuli <samuli.ronkko@gmail.com>
2019-10-13 19:17:59 +0300

    added restaraunts

--- app/src/main/java/com/example/a3lab6_2/MainActivity.java
+++ app/src/main/java/com/example/a3lab6_2/MainActivity.java
@@ -1,18 +1,26 @@
 package com.example.a3lab6_2;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.DatePicker;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -28,11 +36,16 @@ public class MainActivity extends AppCompatActivity implements FetchJSON.MyInter
 
     ListView listView;
     TextView dateTextView;
+    TextView titleTextView;
     FloatingActionButton myFab;
     ArrayList<ArrayList<Meal>> mealArrayLists;
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    NavigationView navigationView;
     int yearDate;
     int monthDate;
     int dayDate;
+    int restaurantId = 66287;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +53,7 @@ public class MainActivity extends AppCompatActivity implements FetchJSON.MyInter
         setContentView(R.layout.activity_main);
         listView = findViewById(R.id.listView);
         listView.setOnItemClickListener(this);
+        titleTextView = findViewById(R.id.titleTextView);
 
         myFab = findViewById(R.id.myFAB);
         myFab.setOnClickListener(this);
@@ -47,6 +61,53 @@ public class MainActivity extends AppCompatActivity implements FetchJSON.MyInter
         setUrl();
 
         dateTextView = findViewById(R.id.dateTextView);
+
+        drawerLayout = findViewById(R.id.activity_main);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, 0, 0);
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        navigationView = (NavigationView)findViewById(R.id.nv);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch(id)
+                {
+                    case R.id.kotkanpoika:
+                        //Toast.makeText(MainActivity.this, "My Account",Toast.LENGTH_SHORT).show();
+                        restaurantId = 66287;
+                        titleTextView.setText("Ravintola Kotkanpoika");
+                        break;
+                    case R.id.wallu:
+                        //Toast.makeText(MainActivity.this, "Settings",Toast.LENGTH_SHORT).show();
+                        restaurantId = 66275;
+                        titleTextView.setText("Ravintola Wallu");
+                        break;
+                    case R.id.alwari:
+                        //Toast.makeText(MainActivity.this, "Settings",Toast.LENGTH_SHORT).show();
+                        restaurantId = 66264;
+                        titleTextView.setText("Ravintola Alwari");
+                        break;
+                    case R.id.fasaani:
+                        //Toast.makeText(MainActivity.this, "Settings",Toast.LENGTH_SHORT).show();
+                        restaurantId = 66302;
+                        titleTextView.setText("Ravintola Fasaani");
+                        break;
+                    default:
+                        return true;
+                }
+                setUrl();
+                drawerLayout.closeDrawer(GravityCompat.START);
+
+                return true;
+
+            }
+        });
+
     }
 
 
@@ -72,7 +133,7 @@ public class MainActivity extends AppCompatActivity implements FetchJSON.MyInter
             Log.d("tag", yearDate + " " + monthDate + " " + dayDate);
         }
         Log.d("tag", yearDate + " " + monthDate + " " + dayDate);
-        url = "https://www.amica.fi/api/restaurant/menu/day?date=" + yearDate + "-" + monthDate + "-" + dayDate+ "&language=en&restaurantPageId=66287";
+        url = "https://www.amica.fi/api/restaurant/menu/day?date=" + yearDate + "-" + monthDate + "-" + dayDate + "&language=fi&restaurantPageId=" + restaurantId;
         fetchJSON();
     }
 
@@ -111,6 +172,15 @@ public class MainActivity extends AppCompatActivity implements FetchJSON.MyInter
         FetchJSON fetchJSON = new FetchJSON(this, url, this);
         fetchJSON.start();
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(actionBarDrawerToggle.onOptionsItemSelected(item))
+            return true;
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
